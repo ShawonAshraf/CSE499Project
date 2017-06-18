7cbfffca1c76a462dbbb8cfe22b118ce9883d7e9
ShawonAshraf <shawon13@live.com>
2017-06-18 13:01:03 +0600

    modified label_image.py

--- BasicClassifier/label_image.py
+++ BasicClassifier/label_image.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# change this as you see fit
+# input image path as sysarg
 image_path = sys.argv[1]
 
 # Read in the image_data
