06728b79236f429c0262b40fb8a39d18d2a6e579
Shawon Ashraf <ShawonAshraf@users.noreply.github.com>
2017-07-03 01:23:30 +0600

    classifier as a class framework

--- FruitClassifier/Classifier.py
+++ FruitClassifier/Classifier.py
@@ -1,10 +1,8 @@
 import os
-import time
-import sys
 import tensorflow as tf
 
-
-
+from matplotlib import pyplot as plt
+from matplotlib import image as mat_img
 
 
 class Classifier:
@@ -18,39 +16,56 @@ class Classifier:
         return 'Image Classifier : {}'.format(type(self))
 
     def classify_fruit(self):
-        # read in image data
-        image_data = tf.gfile.FastGFile(self.img_path, 'rb').read()
+        try:
+            # read in image data
+            image_data = tf.gfile.FastGFile(self.img_path, 'rb').read()
 
-        # load labels
-        label_lines = [line.restrip() for line
-                       in tf.gfile.FastGFile('retrained_labels.txt')]
+            # load labels
+            label_lines = [line.rstrip() for line
+                           in tf.gfile.FastGFile('../BasicClassifier/retrained_labels.txt')]
 
-        # graph from file
+            # graph from file
 
-        graph_file = tf.gfile.FastGFile('retrained_graph.pb', 'rb')
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(graph_file.read())
-        tf.import_graph_def(graph_def, name='')
+            graph_file = tf.gfile.FastGFile('../BasicClassifier/retrained_graph.pb', 'rb')
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(graph_file.read())
+            tf.import_graph_def(graph_def, name='')
 
-        # create tf session
+            # create tf session
 
-        sess = tf.Session()
-        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+            sess = tf.Session()
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-        predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+            predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
 
-        # sorts the result in descending order
-        top_predict = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            # sorts the result in descending order
+            top_predict = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
-        print('Result for {} :\n'.format(self.image_name))
-        node_id = 0 # gets the best result since it's sorted
+            print('Result for {} :\n'.format(self.image_name))
+            node_id = 1  # gets the best result since it's sorted
 
-        label_string = label_lines[node_id]
-        confidence_score = predictions[0][node_id]
+            label_string = label_lines[node_id]
+            confidence_score = predictions[0][node_id]
 
-        print('Label : {}\t Score : {}\n\n'.format(label_string, confidence_score))
+            print('Label : {}\t Score : {}\n\n'.format(label_string, confidence_score))
+
+        except Exception:
+            print('Error! Image not found')
 
 
+    # plots the image using matplotlib
     def plot_img(self):
-        pass
+        image = mat_img.imread(self.img_path)
+        plt.axis('off')
+        plt.imshow(image)
+        plt.show()
 
+
+
+
+# test
+
+img_path = '../BasicClassifier/img_test/gvr.jpg'
+cls = Classifier(img_path)
+cls.classify_fruit()
+# cls.plot_img()
