d3700a588c549d1573920911d8738415ab80fc13
Shawon Ashraf <ShawonAshraf@users.noreply.github.com>
2017-07-03 23:17:36 +0600

    removed main.py

--- src/FruitClassifier/Classifier.py
+++ src/FruitClassifier/Classifier.py
@@ -78,3 +78,14 @@ class Classifier:
         plt.axis('off')
         plt.imshow(image)
         plt.show()
+
+
+# test
+
+img_path = '../../img_test/green_mangoes.jpg'
+cls = Classifier(img_path)
+
+fruit, score = cls.classify_fruit()
+print('Result for image = {}: \n'.format(cls.image_name))
+print('Fruit : {}\nConfidence Score : {}\n\n'.format(fruit, score))
+# cls.plot_img()
