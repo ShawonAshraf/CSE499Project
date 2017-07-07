192b0d2c65663046dabb7010cdf17b8699ff3665
Shawon Ashraf <ShawonAshraf@users.noreply.github.com>
2017-07-08 02:14:18 +0600

    added imageloader for gui

--- macOS_GUI/macOS_GUI/ViewController.swift
+++ macOS_GUI/macOS_GUI/ViewController.swift
@@ -10,6 +10,18 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var imageView: NSImageView!
+    @IBOutlet var loadImageButton: NSButton!
+    @IBOutlet var recognizeButton: NSButton!
+    @IBOutlet var resultLabel: NSTextField!
+    
+    
+    var imagePath: String!
+    var labelPath: String!
+    var graphPath: String!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +33,42 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    
+    @IBAction func loadImage(_ sender: Any) {
+        // create the file open dialog
+        
+        let fileOpenDialog = NSOpenPanel()
+        
+        fileOpenDialog.title = "Select the image file"
+        fileOpenDialog.showsResizeIndicator = true
+        fileOpenDialog.canChooseFiles = true
+        fileOpenDialog.canChooseDirectories = true
+        fileOpenDialog.canCreateDirectories = true
+        fileOpenDialog.allowsMultipleSelection = false
+        fileOpenDialog.allowedFileTypes = ["png", "jpg", "jpeg"]
+        
+        // now call the dialog
+        
+        if (fileOpenDialog.runModal() == NSModalResponseOK) {
+            let result = fileOpenDialog.url
+            
+            if (result != nil) {
+                imagePath = result!.path
+                
+                // set image to image viewer
+                let image = NSImage(byReferencingFile: imagePath!)
+                imageView.image = image
+            }
+        }
+        
+    }
 
+    
+    @IBAction func recognizeFromImage(_ sender: Any) {
+        
+    }
+    
 
 }
 
