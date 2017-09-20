de041883b7b7afaf047e9da7d0a350aa10e7e886
ShawonAshraf <shawon13@live.com>
2017-09-21 01:01:50 +0600

    modified api_tester.py

--- REST_Service/api_tester.py
+++ REST_Service/api_tester.py
@@ -1,22 +1,25 @@
-import base64
 import sys
 import requests, json
+import cv2
 
 
 # pass the image path as sys arg
 image = sys.argv[1]
 
 # convert image json accepted format
+img = cv2.imread(image)
 
-
+# encode image
+_, img_encoded = cv2.imencode(".jpg", img)
+data = img_encoded.tostring()
 # prepare http request headers
+content_type = "/image/jpeg"
+headers = {"content-type": content_type}
 
-
-
-# create json dump
+# define url for service
 url = "http://localhost:9999/api"
-data = ""
+
 
 # send and get the reponse
-response = requests.post(url=url, data=data)
+response = requests.post(url=url, data=data, headers=headers)
 print(json.loads(response.text))
