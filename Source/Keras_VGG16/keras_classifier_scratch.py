1633730b274dd1073e7296cb24f2805940c39a89
Shawon Ashraf <shawon13@live.com>
2017-11-19 02:22:25 +0600

    binary classifier

--- Source/Keras_VGG16/keras_classifier_scratch.py
+++ Source/Keras_VGG16/keras_classifier_scratch.py
@@ -1,42 +1,40 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
-from keras import losses, optimizers, metrics
+from keras import backend as K
 
 # dataset dir
 dataset_root = "img_data/train/"
+validation_root = "img_data/validation/"
 preview_dir = "img_data/preview/"
 
-# create generator
-datagen = ImageDataGenerator(
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode="nearest")
+# number of train and validation samples
+n_train = int(1313 * 0.80)
+n_valid = 1313 - n_train
 
-img = load_img(dataset_root + "fresh_apple/fresh_apple_2.jpg")
-x = img_to_array(img)
-x = x.reshape((1,) + x.shape)
-
-i = 0
-for batch in datagen.flow(x, batch_size=1, save_to_dir=preview_dir, save_prefix="fresh_apple", save_format="jpeg"):
-    i += 1
-    if i > 20:
-        break
-
-
+# image dim
+img_width, img_height = 150, 150
 
 # create the model
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
 model = Sequential()
 
-model.add(Conv2D(32, (3,3), input_shape=(3, 150, 150)))
+# layer 1
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3,3)))
+# layer 2
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# layer 3
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -46,6 +44,30 @@ model.add(Dense(64))
 model.add(Activation("relu"))
 model.add(Dropout(0.5))
 model.add(Dense(1))
-model.add(Activation("sigmoid"))
+model.add(Activation("softmax"))
 
-model.compile(loss=losses.mean_squared_error, optimizer=optimizers.sgd, metrics=metrics.categorical_accuracy)
+model.compile(loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["categorical_accuracy"])
+
+# data preparation
+batch_size = 16
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+# generators for train and validation
+train_generator = train_datagen.flow_from_directory(dataset_root, target_size=(img_width, img_height),
+                                                    class_mode="categorical",
+                                                    batch_size=batch_size)
+validation_generator = test_datagen.flow_from_directory(validation_root, target_size=(img_width, img_height),
+                                                        batch_size=batch_size,
+                                                        class_mode="categorical")
+
+# train
+model.fit_generator(train_generator, steps_per_epoch=n_train // batch_size, epochs=50,
+                    validation_data=validation_generator, validation_steps=n_valid // batch_size)
+model.save_weights("saved_models/train_1.h5")
