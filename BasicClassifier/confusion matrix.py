6752ee6f535eec2b4abf224ed8d4b993f19c814a
Shawon Ashraf <shawon13@live.com>
2017-11-26 17:02:30 +0600

    ipnb conf matrix

--- BasicClassifier/confusion matrix.py
+++ BasicClassifier/confusion matrix.py
@@ -1,6 +1,6 @@
 import os, sys
 import tensorflow as tf
-import time
+from sklearn.metrics import confusion_matrix
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -10,7 +10,7 @@ def classify(image_path):
     image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
     # Loads label file, strips off carriage return
-    label_lines = [line.rstrip() for line 
+    label_lines = [line.rstrip() for line
                    in tf.gfile.GFile("retrained_labels.txt")]
 
     # label_names = label_lines
@@ -24,25 +24,27 @@ def classify(image_path):
     with tf.Session() as sess:
         # Feed the image_data as input to the graph and get first prediction
         softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-    
+
         predictions = sess.run(softmax_tensor, \
-             {'DecodeJpeg/contents:0': image_data})
+                               {'DecodeJpeg/contents:0': image_data})
 
         # global_pred = predictions
-    
+
         # Sort to show labels of first prediction in order of confidence
         top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
         # label_id = top_k
-    
+
         print('Result for {} :\n'.format(image_path))
-        for node_id in top_k:
-            human_string = label_lines[node_id]
-            score = predictions[0][node_id]
-            print('Category : {} \nScore = {} %'.format(human_string, score * 100))
+        # for node_id in top_k:
+        #     human_string = label_lines[node_id]
+        #     score = predictions[0][node_id]
+        #     print('Category : {} \nScore = {} %'.format(human_string, score * 100))
+        node_id = top_k[0]
+        print('Category : {}\tScore : {}'.format(label_lines[node_id], predictions[0][node_id]))
 
         print('\nDONE===========================', end='\n')
 
-        return label_lines, predictions, top_k
+        return label_lines, predictions[0], top_k
 
 
 # get confusion matrix
@@ -60,23 +62,12 @@ def get_cnf_matrix(label, pred):
 image_test_root_dir = 'img_test/'
 test_image_list = os.listdir(image_test_root_dir)
 
-
-# for image in test_image_list:
-#     try:
-#         print('Input image = {}'.format(image))
-#         init_time = time.time()
-#         image_path = os.path.join(image_test_root_dir, image)
-#         l, p, k = classify(image_path)
-#         end_time = time.time()
-#         exec_time = end_time - init_time
-#         print(" in {} s\n\n".format(exec_time))
-#         get_cnf_matrix(l, p)
-#     except FileNotFoundError:
-#         print('{} not found\n\n'.format(image))
-
-
 # test for one image
 test_image = os.path.join(image_test_root_dir, test_image_list[0])
 l, p, k = classify(test_image)
 
-get_cnf_matrix(k, p[0])
+node_id = k[0]
+ground_truth_id = 5
+
+cnf = confusion_matrix(y_true=[5], y_pred=[k[0]])
+print(cnf)
--- BasicClassifier/train.sh
+++ BasicClassifier/train.sh
@@ -1,8 +1,8 @@
 python retrain.py \
   --bottleneck_dir=bottlenecks \
-  --how_many_training_steps=50000 \
+  --how_many_training_steps=5000 \
   --model_dir=inception \
-  --summaries_dir=training_summaries/basic \
+  --summaries_dir=training_summaries_new/basic \
   --output_graph=retrained_graph.pb \
   --output_labels=retrained_labels.txt \
   --image_dir=img_data
